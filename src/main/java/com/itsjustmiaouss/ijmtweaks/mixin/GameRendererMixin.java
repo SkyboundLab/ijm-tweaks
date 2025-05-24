683f70f92a1ef194d9e3170f0db9f3094da9a8b9
Prince527 <Prince527gaming@gmail.com>
2025-05-24 17:34:48 -0300

    No night vision flicker

--- src/main/java/com/itsjustmiaouss/ijmtweaks/mixin/GameRendererMixin.java
+++ src/main/java/com/itsjustmiaouss/ijmtweaks/mixin/GameRendererMixin.java
@@ -6,6 +6,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -37,5 +39,12 @@ public abstract class GameRendererMixin {
             options.smoothCameraEnabled = false;
         }
     }
+
+    // Credit: https://github.com/Bestsoft101/SmallMods/blob/nonightvisionflicker/src/client/java/b100/nonightvisionflicker/mixin/client/GameRendererMixin.java
+	@Inject(method = "getNightVisionStrength", at = @At("HEAD"), cancellable = true)
+	private static void overrideGetNightVisionStrength(LivingEntity entity, float tickDelta, CallbackInfoReturnable<Float> ci) {
+		ci.setReturnValue(entity.hasStatusEffect(StatusEffects.NIGHT_VISION) ? 1.0f : 0.0f);
+		ci.cancel();
+	}
 
 }
