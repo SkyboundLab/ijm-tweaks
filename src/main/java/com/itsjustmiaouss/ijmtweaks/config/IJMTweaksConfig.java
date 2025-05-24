4dad0dcc25275d09d8165db2e1924c593496f3a2
Prince527 <Prince527gaming@gmail.com>
2025-05-24 17:22:25 -0300

    Update IJMTweaksConfig.java

--- src/main/java/com/itsjustmiaouss/ijmtweaks/config/IJMTweaksConfig.java
+++ src/main/java/com/itsjustmiaouss/ijmtweaks/config/IJMTweaksConfig.java
@@ -150,7 +150,7 @@ public class IJMTweaksConfig {
                     .build();
 
             Option<Boolean> fullbrightOpt = IJMTweaksConfig.<Boolean>getGenericOption(
-                    "fullbright", "fullbright", OptionRequirement.NON_SURVIVAL
+                    "fullbright", "fullbright"
                     )
                     .binding(defaults.fullbright,
                             () -> config.fullbright,
